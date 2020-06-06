eab225c5940d77149f99416bf718fd71efb885f3
Narayanan-Sethuramalingam <surya.storm@gmail.com>
2020-06-06 21:37:55 +0400

    Updates

---
+++ precache-manifest.622ec59bae511df4203e6cf1d12c5e94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a55d1186e29a148c05084af341c2ddf0",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "fd8a2dadedae953f6180",
+    "url": "/react-deploy/static/css/main.eff17d9c.chunk.css"
+  },
+  {
+    "revision": "c623d30628c24fefe589",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ca0397b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd8a2dadedae953f6180",
+    "url": "/react-deploy/static/js/main.0a3a350a.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  },
+  {
+    "revision": "ebe40bbee24d0bbf0b2a289f39f7fb42",
+    "url": "/react-deploy/static/media/Family.ebe40bbe.jpg"
+  }
+]);
